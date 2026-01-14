cf80d8488b83070c142e11220a1ddf103b6d7c1d
molle98 <mmollerach98@hotmail.com>
2026-01-14 15:38:06 -0300

    Deploy initial health endpoint

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,28 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const healthLambda = new lambda.Function(this, "HealthLambda", {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "index.handler",
+      code: lambda.Code.fromInline(`
+        exports.handler = async () => ({
+          statusCode: 200,
+          body: JSON.stringify({ status: "ok" })
+        });
+      `),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigw.RestApi(this, "NotesApi", {
+      restApiName: "Notes API",
+    });
+
+    const health = api.root.addResource("health");
+    health.addMethod("GET", new apigw.LambdaIntegration(healthLambda));
   }
 }
